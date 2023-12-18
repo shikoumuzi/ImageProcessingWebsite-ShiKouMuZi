f3c018c194e71665ea9da9bde7d24a2955f6f654
shikoumuzi <1013740747@qq.com>
2023-12-18 21:35:57 +0800

    导入roket 创建LoginVue

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,8 +1,34 @@
+#[macro_use] extern crate rocket;
+
 mod user;
 use user::user as User;
-fn main() {
-    println!("Hello, world!");
-    // let mut user = User::User::new();
-    
+use rocket::fairing::AdHoc;
+
+// #[derive(Deserialize)]
+// struct AppConfig {
+//     id: Option<usize>,
+//     port: u16,
+// }
+
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+
+      POST /
+
+          accepts raw data in the body of the request and responds with a URL of
+          a page containing the body's content
+
+      GET /<id>
+
+          retrieves the content for the paste with id `<id>`
+    "
 }
 
+#[launch]
+fn rocket() -> _ {
+    // rocket::build().attach(AdHoc::config::<AppConfig>())
+    rocket::build().mount("/", routes![index])
+
+}
