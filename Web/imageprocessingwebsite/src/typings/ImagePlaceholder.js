ad2318e1ab01ec19f0dd8c9b8ec0ad9507accdfe
shikoumuzi <1013740747@qq.com>
2024-03-01 23:36:34 +0800

    添加初始化initSymbolFlag函数

--- Web/imageprocessingwebsite/src/typings/ImagePlaceholder.js
+++ Web/imageprocessingwebsite/src/typings/ImagePlaceholder.js
@@ -1,6 +1,25 @@
+/* eslint-disable camelcase */
+import axios from '../plugin/AxiosAPI'
+
 export default class ImagePlaceholder {
     constructor() {
-        this.symbol_id = ''
-        this.src_operation_id = '' // 来源操作符号
+        this.symbol_id = '' // 标识符号 通过 time_stamp user_id 本机IP地址
+        this.src_operation_id = '' // 来源操作id
+    }
+
+    initSymbolID(user_id) {
+        axios.get(
+            'https://api.ipify.org/?format=json'
+        ).then((response) => {
+            if (response.data != null) {
+                const ip = response.data.ip
+                const time_stamp = new Date().valueOf().toString()
+                this.symbol_id = user_id + '|' + time_stamp + '|' + ip
+            }
+        })
+    }
+
+    setSrcOperationID(src_operation_id) {
+        this.src_operation_id = src_operation_id
     }
 }
