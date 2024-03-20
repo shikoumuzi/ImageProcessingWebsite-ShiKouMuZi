aca3f53e6a414f6479b89006112f9a21766252b3
shikoumuzi <1013740747@qq.com>
2024-03-20 13:42:46 +0800

    完成suggestion_api

--- Server/src/backend_api/suggestion_api.rs
+++ Server/src/backend_api/suggestion_api.rs
@@ -1,14 +1,37 @@
 use std::sync::Mutex;
+use std::time::{SystemTime, UNIX_EPOCH};
 use super::super::typings::user::user::User;
 use crate::backend_api::request;
 use rocket::fairing::AdHoc;
 use rocket::{Route, State};
 use rocket::serde::json::Json;
+use rusqlite::params;
+use crate::sqlite::sqlite::SQLite;
 use crate::typings::user::user::UserGroup;
 use super::response::suggestion::suggestion::Response as SuggestionResponse;
 
 #[get("/image_processing_website_api/submit_suggestion?<token>&<content>&<time_stamp>")]
-fn submitSuggestion(users: &State<Mutex<UserGroup>>, token: String, content: String, time_stamp: u64) -> Json<SuggestionResponse>{
+fn submitSuggestion(users: &State<Mutex<UserGroup>>, sqlite: &State<Mutex<SQLite>>, token: String, content: String, time_stamp: u64) -> Json<SuggestionResponse>{
+
+    let mut _users = users.lock().unwrap();
+    let user = _users.find_user_by_token(&token);
+    if user.is_none() {
+        let suggestion_response = SuggestionResponse::new(1);
+        return Json(suggestion_response);
+    }
+
+    let mut _sqlite = sqlite.lock().unwrap();
+    let conn = _sqlite.getConn();
+
+    let execute_result =
+        conn.execute("INSERT INTO SUGGESTION(USER_NAME, SUGGESTION_CONTENT, TIME_STAMP, SUGGECTION_STATE) VALUE (?1, ?2, ?3, ?5)",
+                     params![user.unwrap().username, content, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()]);
+    if execute_result.is_err(){
+        println!("stmt is error {:?}", execute_result);
+        let suggestion_response = SuggestionResponse::new(1);
+        return Json(suggestion_response);
+    }
+
     let suggestion_response = SuggestionResponse::new(0);
     Json(suggestion_response)
 }
